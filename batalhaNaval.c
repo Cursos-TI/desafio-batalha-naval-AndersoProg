da15e34aef53f1374c712ed6173a438a2290edb2
AndersoProg <andersonvideira2015@icloud.com>
2025-02-01 00:14:25 +0000

    criei a matriz 8x8

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,10 +4,26 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
+
+
+
 int main() {
     // Nível Novato - Posicionamento dos Navios
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
+    int matriz [8][8] {
+        {1,  2,  4,   5,  6,  7,  8},
+        {9,  10, 11, 12, 13, 14, 15, 16},
+        {17, 18, 19, 20, 21, 22, 23, 24},
+        {25, 26, 27, 28, 29, 30, 31, 32},
+        {33, 34, 35, 36, 37, 38, 39, 40},
+        {41, 42, 43, 44, 45, 46, 47, 48},
+        {49, 50, 51, 52, 53, 54, 55, 56},
+        {57, 58, 59, 60, 61, 62, 63, 64}
+
+    }
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
+
+    
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
